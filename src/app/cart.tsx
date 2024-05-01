1755411b8c2ef7a5eb77e99d592c210b7d05d486
Joao Sertoli <jgsertoli@gmail.com>
2024-04-30 21:26:49 -0300

    Add react-native-keyboard-aware-scroll-view and @react-native-async-storage/async-storage dependencies

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,39 +1,77 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Alert } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 import { Header } from "@/components/header";
-import { useCartStore } from "@/stores/cart-store";
+import { ProductCartProps, useCartStore } from "@/stores/cart-store";
 import { Product } from "@/components/product"
 import { formatCurrency } from "@/utils/functions/format-currency";
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
+import { LinkButton } from "@/components/link-button";
+import { useState } from "react";
 
 export default function Cart() {
+    const [address, setAddress] = useState("")
     const cartStore = useCartStore();
 
     const total = formatCurrency(cartStore.products.reduce((total, product) => total + product.price * product.quantity, 0));
+
+    function handleProductRemove(product: ProductCartProps) {
+        Alert.alert("Remover", `Deseja remover ${product.title} do carrinho?`, [{
+            text: "Cancelar",
+        }, {
+            text: "Remover",
+            onPress: () => cartStore.remove(product.id)
+        }])
+    }
+
+    function handleOrder() {
+        if (address.trim().length === 0) {
+            return Alert.alert("Pedido", "Informe os dados da entrega: ")
+        }
+
+        const products = cartStore.products.map((product) => `\n ${product.quantity} ${product.title} - ${formatCurrency(product.price * product.quantity)}`).join("")
+    }
 
     return (
         <View className="flex-1 pt-8">
             <Header title="Seu carrinho" />
 
-
-            <ScrollView>
-                {cartStore.products.length > 0 ? (
+            <KeyboardAwareScrollView>
+                <ScrollView>
                     <View className="p-5 flex-1">
-                        {
-                            cartStore.products.map((product) => (
-                                <Product key={product.id} data={product} />
-                            ))
-                        }
+                        {cartStore.products.length > 0 ? (
+                            <View className="border-b border-slate-700">
+                                {cartStore.products.map((product) => (
+                                    <Product key={product.id} data={product} onPress={() => handleProductRemove(product)} />
+                                ))}
+                            </View>
+                        ) : (
+                            <Text className="font-body text-slate-400 text-center my-8" > Seu carrinho está vazio. </Text>
+                        )}
+
+                        <View className="flex-row gap-2 items-center mt-5 mb-4">
+                            <Text className="text-white text-xl font-subtitle pl-1"> Total:  </Text>
+
+                            <Text className="text-lime-400 text-2xl font-heading"> {total} </Text>
+                        </View>
+
+                        <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP e complemento" onChangeText={setAddress} />
                     </View>
-                ) : (
-                    <Text className="font-body text-slate-400 text-center my-8" > Seu carrinho está vazio. </Text>
-                )}
+                </ScrollView>
+            </KeyboardAwareScrollView>
 
-                <View className="flex-row gap-2 items-center mt-5 mb-4">
-                    <Text className="text-white text-xl font-subtitle"> Total:  </Text>
+            <View className="p-5 gap-5">
+                <Button onPress={handleOrder}>
+                    <Button.Text>Enviar Pedido</Button.Text>
+                    <Button.Icon>
+                        <Feather name="arrow-right-circle" size={20} />
+                    </Button.Icon>
+                </Button>
 
-                    <Text className="text-lime-400 text-2xl font-heading"> {total} </Text>
-                </View>
-            </ScrollView>
+                <LinkButton className="text-slate-400" title="Voltar ao cardápio" href="/" />
+            </View>
         </View>
     )
 }
--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -2,6 +2,9 @@ import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
 import * as cartInMemory from "./helpers/cart-in-memory";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createJSONStorage, persist } from "zustand/middleware"
+
 export type ProductCartProps = ProductProps & {
     quantity: number
 }
@@ -9,9 +12,20 @@ export type ProductCartProps = ProductProps & {
 type StandProps = {
     products: ProductCartProps[]
     add: (product: ProductProps) => void
+    remove: (productId: string) => void
 }
 
-export const useCartStore = create<StandProps>((set) => ({
-    products: [],
-    add: (product: ProductProps) => set((state) => ({ products: cartInMemory.add(state.products, product) }))
-}))
+export const useCartStore = create(
+    persist<StandProps>((set) => ({
+        products: [],
+
+
+        add: (product: ProductProps) => set((state) => ({ products: cartInMemory.add(state.products, product) })),
+
+        remove: (productId: string) => set((state) => ({
+            products: cartInMemory.remove(state.products, productId),
+        })),
+    }), {
+        name: "nlw-rn:cart",
+        storage: createJSONStorage(() => AsyncStorage),
+    }))
